cc5c4ca81388f9c0d648fa383f56f6a7e7df1232
vivek_dwivedi <soarvivekdwivedi@gmail.com>
2026-07-02 20:00:40 +0530

    int data type

---
+++ DataTypes/intDataType.py
@@ -0,0 +1,11 @@
+# In Python, the int data type represents integers, which are positive, negative, 
+# or zero whole numbers without any fractional or decimal parts.
+
+digit = 5
+print(digit)
+print(type(digit))
+
+x = int(10)
+
+print(x)
+print(type(x))
