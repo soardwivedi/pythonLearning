194ab7827d5609d44aa5621932b7690553988e96
vivek_dwivedi <soarvivekdwivedi@gmail.com>
2026-07-11 19:32:45 +0530

    mutable immutable

---
+++ DataTypes/aa MutableImutable.py
@@ -0,0 +1,19 @@
+# Mutablle Data types
+
+# List
+# Set
+# Dictionary
+# Bytearray
+# Array
+
+
+# Immutable Data Types
+
+# Integers
+# Floating-point numbers
+# Boolean
+# String
+# Tuples
+# Frozen Set
+# Bytes
+
--- DataTypes/sliceStr.py
+++ DataTypes/sliceStr.py
@@ -1,3 +1,7 @@
 # Get the characters from position 2 to position 5 (not included):
 b = "hello world"
 print(b[2:7])
+
+# Get the characters from the start to position 5 (not included):
+b = "Hellow World"
+print(b)
